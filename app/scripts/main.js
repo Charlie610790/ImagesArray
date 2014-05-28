488536f4235b1485a568d897998aaa938b982e62
Charlie Jones <myupstateproperties@gmail.com>
2014-05-27 23:46:20 -0400

    add images for rotaing players and rework JS functions to operate the rotating player images

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,50 +1,72 @@
-var players = [
-    'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/3748.png&w=350&h=254',//David Ortiz
-    'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/6298.png&w=350&h=254',//Stephen Drew
-    'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/30130.png&w=350&h=254',//Koji Uehara
-    'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/28444.png&w=350&h=254',//Mike Napoli
-    'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/31606.png&w=350&h=254',//Xander Bogarts
-    'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/5882.png&w=350&h=254',//Grady Sizemore
-    'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/28487.png&w=350&h=254',//John Lester
-    'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/5860.png&w=350&h=254',//Johnny Gomes
-    'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/31232.png&w=350&h=254',//Will Middlebrooks
-    'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/32362.png&w=350&h=254',//Jackie Bradley Jr.
-    'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/28855.png&w=350&h=254',//Clay Buchholz
-    'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/5409.png&w=350&h=254',//Shane Victorino
-    'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/6393.png&w=350&h=254'//Dustin Pedroia
-];
+// var players = [
+//     'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/3748.png&w=350&h=254',//David Ortiz
+//     'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/6298.png&w=350&h=254',//Stephen Drew
+//     'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/30130.png&w=350&h=254',//Koji Uehara
+//     'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/28444.png&w=350&h=254',//Mike Napoli
+//     'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/31606.png&w=350&h=254',//Xander Bogarts
+//     'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/5882.png&w=350&h=254',//Grady Sizemore
+//     'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/28487.png&w=350&h=254',//John Lester
+//     'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/5860.png&w=350&h=254',//Johnny Gomes
+//     'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/31232.png&w=350&h=254',//Will Middlebrooks
+//     'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/32362.png&w=350&h=254',//Jackie Bradley Jr.
+//     'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/28855.png&w=350&h=254',//Clay Buchholz
+//     'http://1.bp.blogspot.com/-oBOoBntSHTI/Ul2Fr9UuWkI/AAAAAAAAB2A/8Hgolfw5lgo/s1600/LACK.png',//John Lackey
+//     'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/30910.png&w=350&h=254',//Daniel Nava
+//     'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/29660.png&w=350&h=254',//Mike Carp
+//     'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/30066.png&w=350&h=254',//Felix Doubrant
+//     'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/31039.png&w=350&h=254',//Ryan Lavarnway
+//	   'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/31257.png&w=350&h=254',//Drake Britton
+//	   'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/5409.png&w=350&h=254',//Shane Victorino
+//     'http://a.espncdn.com/combiner/i?img=/i/headshots/mlb/players/full/6393.png&w=350&h=254'//Dustin Pedroia
+// ];
 
-// function Player (name,imgURL) {
-//     this.imageURL = imgURL;
-//     this.playerName = name;
+function Player (name,imgURL) {
+    this.imageURL = imgURL;
+    this.playerName = name;
+}
+
+window.onload = function() {
+    var image=document.getElementById("picSpot");
+    var img_array=['../images/jonLester.png', '../images/davidOrtiz.png', '../images/stephenDrew.png', '../images/mikeNapoli.png', '../images/kojiUehara.png', '../images/xanderBogarts.png', '../images/gradySizemore.png', '../images/johnnyGomes.png', '../images/willMiddlebrooks.png', '../images/jackieBradley.png', '../images/clayBuchholz.png', '../images/shaneVictorino.png', '../images/johnLackey.png', '../images/danielNava.png', '../images/mikeCarp.png', '../images/felixDoubrant.png', '../images/ryanLavarnway.png', '../images/drakeBritton.png'];
+    var index=0;
+    var interval = 2000;
+    function slide() {
+        image.src = img_array[index++%img_array.length];
+    }
+
+    setInterval(slide, interval);
+}
+
+// var img = 0;
+
+// $.each(players,function(i,e)
+// 	{var redSoxPlayer = new Image();
+// 		redSoxPlayer.src=e;
+// });
+
+// $('img').attr('src',players[0]);
+
+// $('.container').click(function(){
+//     $('img').fadeOut('slow',function(){
+//         $(this).attr('src',players[(++img)%players.length]).fadeIn('slow').delay(800);
+//     });
+// });
+
+// var images = ['../images/johnlester.png', '../images/davidortiz.png', '../images/stephendrew.png', '../images/mikenapoli.png'];
+// var current = 4;
+
+// function rotate() {
+//     document.getElementById('image') = [current];
+//     current = (current + 1) % images.length;
+
+// }
+
+// window.onload = function() {
+//     rotate();
+//     setInterval(rotate, 5000);
 // }
 
 //     $('.image').append('<div style="background-image: url(' + players[0].imageURL + '); height: 254px;"></div>');
 //     $('.image').append('<div style="background-image: url(' + players[1].imageURL + '); height: 254px;"></div>');
 //     $('.image').append('<div style="background-image: url(' + players[2].imageURL + '); height: 254px;"></div>');
 //     $('.image').append('<div style="background-image: url(' + players[3].imageURL + '); height: 254px;"></div>');
-
-var img = 0;
-
-// preload images
-$.each(players,function(i,e)
-	{var redSoxPlayer = new Image();
-		redSoxPlayer.src=e;
-
-});
-
-// populate the image with first entry
-$('img').attr('src',players[0]);
-
-// standard jQuery
-$('.container').click(function(){
-    $('img').fadeOut('slow',function(){
-        $(this).attr('src',players[(++img)%players.length]).fadeIn('slow').delay(800);
-    });
-});
-// var opacity = 0.1; // change this for minimum opacity
-// $('#fadeTo').click(function(){
-//     $('img').fadeTo('slow',opacity,function(){
-//         $(this).attr('src',imgs[(++img)%imgs.length]).fadeTo('slow',1);
-//     });
-// });
