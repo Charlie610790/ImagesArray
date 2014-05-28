516c3b9a4628e0224ab6541c4b0786b796070ebf
Charlie Jones <myupstateproperties@gmail.com>
2014-05-28 13:26:33 -0400

    fix image links to ./images/

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,71 +1,50 @@
 'use strict';
 //Array of player images
-var players = [
-    {
-     playerName: Jon Lester;
-     imageURL: '../images/jonLester.png';
-    },
+// var players = [
+//     {
+//      playerName: Jon Lester;
+//      imageURL: '../images/jonLester.png';
+//     },
 
-    {
-     playerName: Jon Lester;
-     imageURL: '../images/jonLester.png';
-    },
+//     {
+//      playerName: Jon Lester;
+//      imageURL: '../images/jonLester.png';
+//     },
 
-    {
-     playerName: Jon Lester;
-     imageURL: '../images/jonLester.png';
-    },
+//     {
+//      playerName: Jon Lester;
+//      imageURL: '../images/jonLester.png';
+//     },
 
-    {
-     playerName: Jon Lester;
-     imageURL: '../images/jonLester.png';
-    },
+//     {
+//      playerName: Jon Lester;
+//      imageURL: '../images/jonLester.png';
+//     },
 
-    {
-     playerName: Jon Lester;
-     imageURL: '../images/jonLester.png';
-    },
+//     {
+//      playerName: Jon Lester;
+//      imageURL: '../images/jonLester.png';
+//     },
 
-    {
-     playerName: Jon Lester;
-     imageURL: '../images/jonLester.png';
-    },
+//     {
+//      playerName: Jon Lester;
+//      imageURL: '../images/jonLester.png';
+//     },
 
-    {
-     playerName: Jon Lester;
-     imageURL: '../images/jonLester.png';
-    },
+//     {
+//      playerName: Jon Lester;
+//      imageURL: '../images/jonLester.png';
+//     },
 
-    {
-     playerName: Jon Lester;
-     imageURL: '../images/jonLester.png';
-    },
+//     {
+//      playerName: Jon Lester;
+//      imageURL: '../images/jonLester.png';
+//     },
 
-    {
-     playerName: Jon Lester;
-     imageURL: '../images/jonLester.png';
-    },
-    '../images/jonLester.png',
-    '../images/davidOrtiz.png',
-    '../images/stephenDrew.png',
-    '../images/mikeNapoli.png',
-    '../images/kojiUehara.png',
-    '../images/xanderBogarts.png',
-    '../images/gradySizemore.png',
-    '../images/johnnyGomes.png',
-    '../images/willMiddlebrooks.png',
-    '../images/jackieBradley.png',
-    '../images/clayBuchholz.png',
-    '../images/shaneVictorino.png',
-    '../images/johnLackey.png',
-    '../images/danielNava.png',
-    '../images/mikeCarp.png',
-    '../images/felixDoubrant.png',
-    '../images/ryanLavarnway.png',
-    '../images/drakeBritton.png'
-];
-
-// var imgArray = [
+//     {
+//      playerName: Jon Lester;
+//      imageURL: '../images/jonLester.png';
+//     },
 //     '../images/jonLester.png',
 //     '../images/davidOrtiz.png',
 //     '../images/stephenDrew.png',
@@ -85,6 +64,27 @@ var players = [
 //     '../images/ryanLavarnway.png',
 //     '../images/drakeBritton.png'
 // ];
+
+var imgArray = [
+    './images/jonLester.png',
+    './images/davidOrtiz.png',
+    './images/stephenDrew.png',
+    './images/mikeNapoli.png',
+    './images/kojiUehara.png',
+    './images/xanderBogarts.png',
+    './images/gradySizemore.png',
+    './images/johnnyGomes.png',
+    './images/willMiddlebrooks.png',
+    './images/jackieBradley.png',
+    './images/clayBuchholz.png',
+    './images/shaneVictorino.png',
+    './images/johnLackey.png',
+    './images/danielNava.png',
+    './images/mikeCarp.png',
+    './images/felixDoubrant.png',
+    './images/ryanLavarnway.png',
+    './images/drakeBritton.png'
+];
 
 //Player constructor
 function Player (name,imgURL) {
