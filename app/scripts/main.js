2c73da1114ff13ff27ca7877aadec75cf0687ebb
Charlie Jones <myupstateproperties@gmail.com>
2014-05-28 13:37:37 -0400

    changed image paths

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -66,24 +66,24 @@
 // ];
 
 var imgArray = [
-    '/images/jonLester.png',
-    '/images/davidOrtiz.png',
-    '/images/stephenDrew.png',
-    '/images/mikeNapoli.png',
-    '/images/kojiUehara.png',
-    '/images/xanderBogarts.png',
-    '/images/gradySizemore.png',
-    '/images/johnnyGomes.png',
-    '/images/willMiddlebrooks.png',
-    '/images/jackieBradley.png',
-    '/images/clayBuchholz.png',
-    '/images/shaneVictorino.png',
-    '/images/johnLackey.png',
-    '/images/danielNava.png',
-    '/images/mikeCarp.png',
-    '/images/felixDoubrant.png',
-    '/images/ryanLavarnway.png',
-    '/images/drakeBritton.png'
+    'images/jonLester.png',
+    'images/davidOrtiz.png',
+    'images/stephenDrew.png',
+    'images/mikeNapoli.png',
+    'images/kojiUehara.png',
+    'images/xanderBogarts.png',
+    'images/gradySizemore.png',
+    'images/johnnyGomes.png',
+    'images/willMiddlebrooks.png',
+    'images/jackieBradley.png',
+    'images/clayBuchholz.png',
+    'images/shaneVictorino.png',
+    'images/johnLackey.png',
+    'images/danielNava.png',
+    'images/mikeCarp.png',
+    'images/felixDoubrant.png',
+    'images/ryanLavarnway.png',
+    'images/drakeBritton.png'
 ];
 
 //Player constructor
